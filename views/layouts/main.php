8b056ceb44337b2620e3bb4e21037895e4004371
ElisDN <mail@elisdn.ru>
2016-04-29 16:39:39 +0300

    Added alert widget

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -3,6 +3,7 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
+use app\widgets\Alert;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
@@ -70,6 +71,7 @@ AppAsset::register($this);
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
+        <?= Alert::widget() ?>
         <?= $content ?>
     </div>
 </div>
